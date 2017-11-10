47d480a1214e83bd303e2d2c3d76c5f411e4e6b0
Allart Kooiman <akooiman@nieuwland.nl>
2017-11-10 15:23:12 +0100

    livereload and alpaca

--- run.js
+++ run.js
@@ -2,13 +2,19 @@ const connect = require('connect');
 const serveStatic = require('serve-static');
 const path = require('path');
 const livereload = require('livereload');
-
+const nodemon = require('nodemon');
 
 var server = connect();
 
 server.use(serveStatic(path.join(__dirname,'example')));
 server.use(serveStatic(path.join(__dirname, 'dist')));
 server.listen(3000);
-const lrserver = livereload.createServer();
+const lrserver = livereload.createServer({
+  delay: 200
+});
 lrserver.watch([path.join(__dirname,'example')]);
+lrserver.watch([path.join(__dirname,'dist')]);
 console.log('http://localhost:3000');
+
+
+nodemon('-x "npm run build"');
---
+++ src/bundle.js
@@ -0,0 +1,4 @@
+window.$ = window.jQuery = require('jquery');
+require('bootstrap');
+require('handlebars');
+require('../node_modules/alpaca/dist/alpaca/bootstrap/alpaca.js');
