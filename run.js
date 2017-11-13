8a65b88f85efafe68a88207b9c1df8959a8b2569
Allart Kooiman <akooiman@nieuwland.nl>
2017-11-13 09:07:35 +0100

    watchify in run

--- run.js
+++ run.js
@@ -2,13 +2,16 @@ const connect = require('connect');
 const serveStatic = require('serve-static');
 const path = require('path');
 const livereload = require('livereload');
-const nodemon = require('nodemon');
+const watchify = require('watchify');
+const browserify = require('browserify');
+const fs = require('fs');
 
 var server = connect();
 
 server.use(serveStatic(path.join(__dirname,'example')));
 server.use(serveStatic(path.join(__dirname, 'dist')));
 server.listen(3000);
+// https://github.com/napcs/node-livereload/issues/68
 const lrserver = livereload.createServer({
   delay: 200
 });
@@ -17,4 +20,16 @@ lrserver.watch([path.join(__dirname,'dist')]);
 console.log('http://localhost:3000');
 
 
-nodemon('-x "npm run build"');
+var b = browserify({
+  entries: ['./src/bundle.js'],
+  cache: {},
+  packageCache: {},
+  plugin: [watchify],
+  debug: true
+});
+
+b.bundle().pipe(fs.createWriteStream('dist/bundle.js'));
+
+b.on('update', function (ids) {
+  console.log('updated');
+});
--- src/bundle.js
+++ src/bundle.js
@@ -2,3 +2,4 @@ window.$ = window.jQuery = require('jquery');
 require('bootstrap');
 require('handlebars');
 require('../node_modules/alpaca/dist/alpaca/bootstrap/alpaca.js');
+alert('hello');
